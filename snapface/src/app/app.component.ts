07136d6b0ea41bf917a9120cae6e88d6a48798b9
Mgoumane <goumane.mahmoudou@gmail.com>
2023-05-18 14:31:27 +0200

    ajout du dossier backend

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, filter, interval, map } from 'rxjs';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
 
-	ngOnInit(): void {}
+  interval$!: Observable<number>;
+
+  ngOnInit(): void {
+    this.interval$ = interval(1000).pipe(
+
+      filter(value => value % 3 === 0),
+
+      map(value => value * 100),
+
+    );
+  }
 
 }
